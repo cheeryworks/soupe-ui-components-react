3f2061e63ef5d6a7c9427dd44154f035928df1bd
Carr Chang <carr.z.chang@outlook.com>
2020-08-20 12:43:41 +0800

    Init docs

---
+++ build/precache-manifest.0f6cd170c57cc609c5269103ee8decf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ccd2e5fef4271b8f3178a4689c6987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6797e4e06aa2c11fb333",
+    "url": "/static/css/main.32821154.chunk.css"
+  },
+  {
+    "revision": "676d4e52b88066b65bc0",
+    "url": "/static/js/2.d03faa4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d03faa4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6797e4e06aa2c11fb333",
+    "url": "/static/js/main.daec8d1e.chunk.js"
+  },
+  {
+    "revision": "178074fe0fe02dfc9779",
+    "url": "/static/js/runtime-main.18491c65.js"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
