f4500a6193ec4d21df970e064f4cc90eec01b678
Carr Chang <carr.z.chang@outlook.com>
2021-05-22 09:49:14 +0800

    Add GitHub link

--- src/demos/App.tsx
+++ src/demos/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HashRouter, NavLink, Route, Switch } from 'react-router-dom'
 import logo from './assets/logo.png'
+import externalLinkIcon from './assets/external-link-alt-solid.svg'
 import './App.sass'
 import Home from './Home'
 import Components from './Components'
@@ -8,33 +9,33 @@ import Components from './Components'
 function App() {
   return (
     <HashRouter>
-      <div className='hero soupe-ui-layout-container'>
+      <div className="hero soupe-ui-layout-container">
         <nav
-          className='hero-head navbar soupe-ui-layout-header is-dark'
-          role='navigation'
-          aria-label='main navigation'
+          className="hero-head navbar soupe-ui-layout-header is-dark"
+          role="navigation"
+          aria-label="main navigation"
         >
-          <div className='navbar-brand'>
-            <NavLink className='navbar-item' to='/'>
-              <img src={ logo } className='logo' alt=''></img>
+          <div className="navbar-brand">
+            <NavLink className="navbar-item" to="/">
+              <img src={ logo } className="logo" alt=""></img>
               Soupe UI Components (React)
             </NavLink>
           </div>
-          <div className='navbar-menu' id='navbar-menu'>
-            <div className='navbar-start'>
+          <div className="navbar-menu" id="navbar-menu">
+            <div className="navbar-start">
               <NavLink
-                className='navbar-item'
-                to='/'
+                className="navbar-item"
+                to="/"
                 exact
-                activeClassName='is-active'
+                activeClassName="is-active"
               >
                 Home
               </NavLink>
               <NavLink
-                className='navbar-item'
-                to='/components'
+                className="navbar-item"
+                to="/components"
                 exact
-                activeClassName='is-active'
+                activeClassName="is-active"
                 isActive={ (match, location) => {
                   if (location.pathname.startsWith('/components')) {
                     return true
@@ -45,20 +46,24 @@ function App() {
               >
                 Components
               </NavLink>
+              <a className="navbar-item" href="https://github.com/cheeryworks/soupe-ui-components-react">
+                GitHub
+                <img src={ externalLinkIcon } className="external-link-icon"/>
+              </a>
             </div>
           </div>
         </nav>
-        <div className='hero-body px-0 py-0 soupe-ui-layout-body hero'>
+        <div className="hero-body px-0 py-0 soupe-ui-layout-body hero">
           <Switch>
-            <Route path='/components'>
+            <Route path="/components">
               <Components/>
             </Route>
-            <Route path='/'>
+            <Route path="/">
               <Home/>
             </Route>
           </Switch>
         </div>
-        <div className='hero-foot px-1 py-1 soupe-ui-layout-footer'>
+        <div className="hero-foot px-1 py-1 soupe-ui-layout-footer">
           ©{ new Date().getFullYear() } Beijing CheeryWorks Technology Co., Ltd.
         </div>
       </div>
